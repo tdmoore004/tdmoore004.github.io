da6c8231a2b35cc4971d8d7a3ae98f2baaf74f1c
tdmoore004 <tdmoore004@gmail.com>
2021-07-17 18:44:05 -0600

    Adjusted styling.

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
         <Header/>
           <Switch>
             <Route exact path="/" component={About}/>
-            {/* <Route exact path="/about" component={About}/> */}
+            <Route exact path="/about" component={About}/>
             <Route exact path="/portfolio" component={Portfolio}/>
             <Route exact path={["/portfolio/traveler", "/portfolio/furlodge", "/portfolio/boredom-buster", "/portfolio/devour-da-burger", "/portfolio/budget-tracker", "/portfolio/fitness-tracker"]} component={ProjectPage}/>
             <Route exact path="/contact" component={Contact}/>
