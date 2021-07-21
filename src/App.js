78b7f42cf1f4b0206dd07e10c6f2ea9bf77fe501
tdmoore004 <tdmoore004@gmail.com>
2021-07-21 09:00:25 -0600

    Removed incomplete landing page from deployed site.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Header from "./components/Header.jsx"
 import Footer from "./components/Footer.jsx"
 
 // Importing page components for routing.
-import Home from "./pages/Home.jsx"
+// import Home from "./pages/Home.jsx"
 import About from "./pages/About.jsx"
 import Portfolio from "./pages/Portfolio.jsx"
 import ProjectPage from "./pages/ProjectPage.jsx"
@@ -19,7 +19,7 @@ function App() {
       <div className="App">
         <Header/>
           <Switch>
-            <Route exact path="/" component={Home}/>
+            <Route exact path="/" component={About}/>
             <Route exact path="/about" component={About}/>
             <Route exact path="/portfolio" component={Portfolio}/>
             <Route exact path={["/portfolio/traveler", "/portfolio/furlodge", "/portfolio/boredom-buster", "/portfolio/devour-da-burger", "/portfolio/budget-tracker", "/portfolio/fitness-tracker"]} component={ProjectPage}/>
